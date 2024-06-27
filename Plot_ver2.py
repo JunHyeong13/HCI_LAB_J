12b1232f69a402de3813cc92f421a37f066c95f4
Harry1201 <87793197+JunHyeong13@users.noreply.github.com>
2024-06-28 01:10:19 +0900

    new code

--- Plot_ver2.py
+++ Plot_ver2.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd  
 
 # 비디오 파일 경로
-video_path = 'C:/Users/나비/Downloads/Test_Video/Face_1W_A1_S3_landmark.mp4'
+video_path = 'C:/Users/나비/Downloads/Test_Video/Face_1W_A2_S2_landmark.mp4'
 cap = cv2.VideoCapture(video_path)
 
 '''
@@ -15,20 +15,31 @@ fps = cap.get(cv2.CAP_PROP_FPS)
 
 # 비디오 프레임 수 가져오기
 fps = cap.get(cv2.CAP_PROP_FPS)
-# print(fps)
-total_seconds = 10  # 첫 10초만 재생
-total_frames = int(fps * total_seconds)
+# total_seconds = 300   # 첫 10초만 재생 || 60초 * 20분 = 1200
 
-years = np.arange(0, total_seconds, 1/fps) 
-#print(years)
+start_seconds=4*60 # 시작할 부분의 시간
+end_seconds=5*60 # 끝낼 부분의 시간
+total_seconds = end_seconds - start_seconds # 보여줄 전체 시간
+
+total_frames = int(fps * total_seconds)
+# 전체 시간을 기준으로 보여줌
+#years = np.arange(0, total_seconds, 1/fps) 
+
+# 특정 시간에 맞게 보여줌.
+years = np.arange(0, total_seconds, 1/fps)
 
 # 1초 단위가 아닌 프레임 단위
-data_xlse = pd.read_excel('C:/Users/나비/Downloads/Test_Excel/Face_1W_A1_S3.xlsx')
+data_xlse = pd.read_excel('C:/Users/나비/Downloads/Test_Excel/Face_1W_A2_S2.xlsx')
 data = data_xlse['box.width']
 
-# 데이터 10초 가정임. 전체를 돌릴때는 지우셈
-data_trimmed = data[:len(years)]
-fig, ax = plt.subplots()
+
+# 특정 구간을 보여주기 위해 선언.
+start_frame = int(start_seconds * fps)
+end_frame = int(end_seconds * fps)
+data_trimmed = data[start_frame:end_frame]
+
+#data_trimmed = data[:len(years)] # 전체 시간에 한하여 보여줄 수 있도록 함.
+fig, ax = plt.subplots(figsize=(10,5))
 line, = ax.plot(years, data_trimmed, lw=2)  
 
 #이건 전체 데이터를 돌릴 때
@@ -37,28 +48,32 @@ fig, ax = plt.subplots()
 line, = ax.plot(years, data, lw=2)
 '''
 
-y_min = np.floor(data.min())
-y_max = np.ceil(data.max())
-ax.set_ylim(y_min, y_max)
+y_min = np.floor(data.min()) # 주어진 숫자의 소수점 이하를 버리고, 정수 부분만 남기는 함수
+y_max = np.ceil(data.max()) # 인수로 받은 숫자를 반올림하여 반환.
+ax.set_ylim(y_min, y_max) # set_ylim의 경우, y축의 최솟값, 최댓값을 설정. 
 
 # x 축을 5초 단위로 눈금 설정
-xticks = np.arange(0, total_seconds + 1, 1)
+xticks = np.arange(0, total_seconds + 1, 10)
 ax.set_xticks(xticks)
-ax.set_xticklabels(xticks)
+#ax.set_xticklabels(xticks)
+ax.set_xticklabels((xticks + start_seconds).astype(int))
 plt.xticks(rotation=45, ha='right')
 ax.set_xlim(0, total_seconds)
 
 # 빨간색 수직선의 초기화
-red_line = ax.axvline(x=0, color='r')
+red_line = ax.axvline(x=0, color='r') 
 
 # 플롯 초기화 함수
 def init():
-    red_line.set_xdata([0])
+    red_line.set_xdata([0]) # 0
     return red_line,
 
 plt.ion()
 plt.show()
 frame_num = 0
+
+# 비디오 캡처 시작 위치 설정
+cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
 
 while cap.isOpened() and frame_num < total_frames:
     ret, frame = cap.read()
